e2ba43db8672f8907abdb5642e9e012e4c6e12ef
Robey Pointer <robey@twitter.com>
2012-04-18 21:46:15 -0700

    fix bug: only run tests that are actually included

--- src/main/scala/com/twitter/scalatest/ScalaTestMixins.scala
+++ src/main/scala/com/twitter/scalatest/ScalaTestMixins.scala
@@ -5,14 +5,14 @@ import Keys._
 
 object ScalaTestMixins {
   val testSettings = Seq(
-    test <<= (streams, scalaInstance, taskTemporaryDirectory, fullClasspath in Test) map { (s, scala, temp, fcp) =>
+    test <<= (streams, scalaInstance, taskTemporaryDirectory, fullClasspath in Test, exportedProducts in Test) map { (s, scala, temp, fcp, tests) =>
       val classname = Option(System.getenv("SBT_TEST_FORMAT")).getOrElse("graph") match {
         case "graph" => "BarReporter"
         case "quiet" => "QuietReporter"
         case "verbose" => "TextReporter"
       }
-      val classpath = fcp map { _.data.getAbsolutePath() }
-      val args = Array("-p", classpath.mkString(" "), "-r", "com.twitter.scalatest." + classname)
+      val testPaths = tests map { _.data.getAbsolutePath() }
+      val args = Array("-p", testPaths.mkString(" "), "-r", "com.twitter.scalatest." + classname)
 
       val loader = TestFramework.createTestLoader(fcp.map(_.data), scala, IO.createUniqueDirectory(temp))
       val runner = "org.scalatest.tools.Runner"
